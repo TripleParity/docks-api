3df446926a55795da90a355cdd3babff09caa14d
Raymond de Vos <devosray@gmail.com>
2018-03-15 20:09:35 +0200

    Test if Docker socket is mounted before starting application

--- scripts/docks-prod-start.sh
+++ scripts/docks-prod-start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Test if the Docker socket file exists
+if test -S "/var/run/docker.sock";
+then
+    echo "Found Docker socket file.";
+else
+    echo "Docker socket file not found. Please mount /var/run/docker.sock">&2;
+    exit 1;
+fi
+
 DEBUG=docks:* NODE_ENV=development PORT=8080 npm run production
