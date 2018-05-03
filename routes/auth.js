7e02541b1d3ef4bd1506d73525d4d56f36ff5695
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-05-03 11:17:39 +0200

    :construction: Test DB connection - Pass

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,33 @@ const express = require('express');
 const request = require('request');
 const router = express.Router();
 
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres', 'postgres', 'example', {
+    host: 'db',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+
 router.get('/test', function (req, res, next) {
-    res.send('Hello!');
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+            res.send('GOOD');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+            res.send(err);
+        });
 });
 
 module.exports = router;
