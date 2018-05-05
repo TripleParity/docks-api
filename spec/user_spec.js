925ebaf15618909ff1c98f63c092527200a7b55d
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-05-05 15:47:11 +0200

    :construction: Fix some async issues

--- spec/user_spec.js
+++ spec/user_spec.js
@@ -17,7 +17,7 @@ describe("user", function() {
     });
 
 
-    const user = new User(sequelizeSqlite);
+    let user = new User(sequelizeSqlite);
 
     it('should have a connection to the test database', async function() {
 
@@ -28,6 +28,33 @@ describe("user", function() {
             console.error(e);
             expect(false).toBe(true);
         }
+    });
+
+    describe('Given an empty database', function() {
+
+        beforeEach(function() {
+            const sqlite = new Sequelize('database', null, null, {
+                dialect: 'sqlite',
+                storage: ':memory:',
+            });
+
+            user = new User(sqlite);
+        });
+
+        describe('When creating a new User object', function() {
+            it('the schema should be created)', async function() {
+                //expect(user.getDatabaseState()).toBe(true);
+
+                //await user.initDatabase();
+                // TODO(egeldenhuys): Test actual database contents?
+                //expect(user.getDatabaseState()).toBe(true);
+
+                let userInstance = await user.createUser('Fred', 'poppers');
+                expect(userInstance.username).toBe('Fred');
+
+            });
+        });
+
 
     });
 });
