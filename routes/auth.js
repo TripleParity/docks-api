ba42846b8118616666b87f3454c481333f13322a
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-05-05 20:47:47 +0200

    :green_heart: Remove sqlite3 requirement from production code

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,21 @@ const router = express.Router();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('testDB2', 'null', 'null', {
-    dialect: 'sqlite',
-    storage: ':memory:',
+const db = new Sequelize('postgres', 'postgres', 'example', {
+    host: 'db',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
 });
 
-
 router.get('/test', function(req, res, next) {
-    sequelize
+    db
         .authenticate()
         .then(() => {
             console.log('Connection has been established successfully.');
