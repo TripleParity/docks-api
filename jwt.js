db4bd9fbe27f329362849f5697da4c59ee366bc0
Raymond de Vos <u16268696@tuks.co.za>
2018-05-08 22:03:13 +0200

    :heavy_plus_sign: Added express-jwt as dependency

---
+++ jwt.js
@@ -0,0 +1,22 @@
+/*
+ * Simple helper module to build the JWT token used throughout the application
+ */
+
+const jwt = require('express-jwt');
+
+// Function to build the JWT middleware
+const jwtMiddleware =  function (secret) {
+    return new jwt({
+        secret: secret,
+
+        // Extracts the token from the Authorization HTTP header
+        getToken: (req) => {
+            if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+                return req.headers.authorization.split(' ')[1];
+            }
+            return null;
+        },
+    })
+};
+
+module.exports = jwtMiddleware;
