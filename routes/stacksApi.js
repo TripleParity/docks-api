ee8f101fce364ae59e7b551b47fa3175499e4c52
raymond <raymond@raymond-hp.lan>
2018-07-15 23:57:12 +0200

    Decode base64 encoded stack file and save to tmp

--- routes/stacksApi.js
+++ routes/stacksApi.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const fs = require('fs');
 
 /* Api endpoint to build and run a docker-compose file */
 
@@ -10,6 +11,36 @@ router.get('/', function(req, res, next) {
 
 // Deploy a new stack to the Swarm. The stack name should not exist
 router.post('/', function(req, res, next) {
+  if (!req.body.hasOwnProperty('stackName') || req.body['stackName'] === '') {
+    res.status(400).send('Required parameter stackName missing');
+    return;
+  }
+
+  if (!req.body.hasOwnProperty('stackFile') || req.body['stackFile'] === '') {
+    res.status(400).send('Required parameter stackFile missing');
+    return;
+  }
+
+  // Debug print information
+  console.log(req.body.stackName);
+  console.log(req.body.stackFile);
+
+  // Base64 decode stack file
+  const stackFileBuffer = Buffer.from(req.body.stackFile, 'base64');
+
+  // Write stack to actual temporary file
+  // TODO(devosray): GUID file names?
+  fs.writeFile('/tmp/stackFile.yml', stackFileBuffer, function(err) {
+    if (err) {
+      res.status(500).send('Error saving stack file to disk');
+      console.log(err);
+      return;
+    }
+
+    // TODO(devosray): Deploy new stack file?
+    res.status(200).send('Saved file to disk');
+  });
+
   // const composeFileContents = req.body['composeFile'];
   // console.log(composeFileContents);
   //
@@ -30,18 +61,18 @@ router.post('/', function(req, res, next) {
   //     res.write("Compose file saved.");
   // })
 
-  const {exec} = require('child_process');
-  exec('docker-compose -f /app/demo-docker-compose.yml up --build', (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      console.log('Error: ' + err);
-      return;
-    }
-
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
+  // const {exec} = require('child_process');
+  // exec('docker-compose -f /app/demo-docker-compose.yml up --build', (err, stdout, stderr) => {
+  //   if (err) {
+  //     // node couldn't execute the command
+  //     console.log('Error: ' + err);
+  //     return;
+  //   }
+  //
+  //   // the *entire* stdout and stderr (buffered)
+  //   console.log(`stdout: ${stdout}`);
+  //   console.log(`stderr: ${stderr}`);
+  // });
 });
 
 module.exports = router;
