28867d978f7153eaed4ff528aa380d6585b09d91
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-09-19 10:40:16 +0200

    Delete webhooks.
    On !200 return json with message field.

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,11 @@
 const express = require('express');
 let router = new express.Router();
 let dockerEventWatcher = require('../lib/docker_event_watcher');
-let dw = require('../lib/docker_webhook');
+let Webhook = require('../lib/docker_webhook');
 
-router.post('/create', async (req, res, next) => {
+let Webhooks = [];
+
+router.post('/', async (req, res, next) => {
     if (!req.body.hasOwnProperty('url') || req.body['url'] === '') {
         res.status(400).send('Required parameter url missing');
         return;
@@ -14,11 +16,40 @@ router.post('/create', async (req, res, next) => {
         return;
     }
 
+    if (!req.body.hasOwnProperty('name') || req.body['name'] === '') {
+        res.status(400).send('Required parameter name missing');
+        return;
+    }
 
-    let wh = new dw(req.body['url'], req.body['types'], dockerEventWatcher);
+    let wh = new Webhook(req.body['name'], req.body['url'], req.body['types'], dockerEventWatcher);
     wh.init();
 
+    Webhooks.push(wh);
+
     res.status(200).send('Webhook created');
+});
+
+router.delete('/', async (req, res, next) => {
+
+    if (!req.body.hasOwnProperty('name') || req.body['name'] === '') {
+        res.status(400).send('Required parameter name missing');
+        return;
+    }
+
+    let wh = new Webhook(req.body['name'], req.body['url'], req.body['types'], dockerEventWatcher);
+    var j = 0;
+    var se = false;
+
+    Webhooks.forEach(webhook => {
+        if(webhook.name === req.body['name']){
+            Webhooks.splice(j, 1);
+            res.status(200).send('Webhook deleted');
+            se = true;
+        }
+        j++
+    });
+    if(!se)
+        res.status(400).send('Webhook not found');
 });
 
 module.exports = router;
