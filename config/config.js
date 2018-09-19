8111c6010f89aa71c9b41f9bdf01b1a4eb03f637
Raymond de Vos <devosray@gmail.com>
2018-09-19 15:33:29 +0200

    Fixed deprecation warning

--- config/config.js
+++ config/config.js
@@ -1,23 +1,28 @@
+const Sequelize = require('sequelize');
+
 module.exports = {
   "development": {
     "username": "postgres",
     "password": process.env.POSTGRES_PASSWORD,
     "database": "postgres",
     "host": process.env.DOCKS_DB_ADDRESS,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    operatorsAliases: Sequelize.Op,
   },
   "test": {
     "username": "postgres",
     "password": process.env.POSTGRES_PASSWORD,
     "database": "postgres",
     "host": process.env.DOCKS_DB_ADDRESS,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    operatorsAliases: Sequelize.Op,
   },
   "production": {
     "username": "postgres",
     "password": process.env.POSTGRES_PASSWORD,
     "database": "postgres",
     "host": process.env.DOCKS_DB_ADDRESS,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    operatorsAliases: Sequelize.Op,
   }
 }
