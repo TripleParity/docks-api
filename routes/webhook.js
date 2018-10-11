95d678b3b194dff2294364d9fdfb5a4c113df1b3
Connor Armand Du Plooy <connor@localhost.localdomain>
2018-10-11 19:57:10 +0200

    eval

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,57 +1,68 @@
 const express = require('express');
 let router = new express.Router();
-let dew = require('../lib/docker_event_watcher');
+let DockerEventWatcher = require('../lib/docker_event_watcher');
 let Webhook = require('../lib/docker_webhook');
-let deh = require('../lib/docker_event_handler');
 
-let docker_event_watcher = new dew();
-let handler = docker_event_watcher.dockerEventHandler;
-docker_event_watcher.start();
+let dockerEventWatcher = new DockerEventWatcher();
+let handler = dockerEventWatcher.dockerEventHandler;
+dockerEventWatcher.start();
 
-let Webhooks = docker_event_watcher.forward_to;
+let Webhooks = dockerEventWatcher.forward_to;
 
 router.post('/', async (req, res, next) => {
-    if (!req.body.hasOwnProperty('url') || req.body['url'] === '') {
-        res.status(500).send({message: "Required parameter missing"});
-        return;
-      }
-    
-    if (!req.body.hasOwnProperty('types') || req.body['types'] === '') {
-        res.status(500).send({message: "Required parameter missing"});
-        return;
-    }
+  if (!req.body.hasOwnProperty('url') || req.body['url'] === '') {
+    res.status(500).send({message: 'Required parameter missing'});
+    return;
+  }
 
-    if (!req.body.hasOwnProperty('name') || req.body['name'] === '') {
-        res.status(500).send({message: "Required parameter missing"});
-        return;
-    }
+  if (!req.body.hasOwnProperty('types') || req.body['types'] === '') {
+    res.status(500).send({message: 'Required parameter missing'});
+    return;
+  }
 
-    let wh = new Webhook(req.body['name'], req.body['url'], req.body['types'], handler);
-    wh.init();
+  if (!req.body.hasOwnProperty('name') || req.body['name'] === '') {
+    res.status(500).send({message: 'Required parameter missing'});
+    return;
+  }
 
-    Webhooks.push(wh);
+  let wh = new Webhook(req.body['name'], req.body['url'],
+    req.body['types'], handler);
 
-    res.status(200).send({message: "Webhook created!"});
+  if (req.body.hasOwnProperty('slack') || req.body['slack'] === true) {
+    wh.modify = (json) => {
+      return {text: JSON.stringify(json)};
+    };
+  }
+
+  if (req.body.hasOwnProperty('modifier')) {
+    wh.modify = eval(req.body['modifier']);
+  }
+
+  wh.init();
+
+  Webhooks.push(wh);
+
+  res.status(200).send({message: 'Webhook created!'});
 });
 
 router.delete('/', async (req, res, next) => {
+  let name = req.params.name;
 
-    let name = req.params.name;
-    
-    var j = 0;
-    var se = false;
+  let j = 0;
+  let se = false;
 
-    Webhooks.forEach(webhook => {
-        if(webhook.name === name){
-            Webhooks.splice(j, 1);
-            res.status(200).send({message: "Webhook deleted!"});
-            se = true;
-        }
-        j++
-    });
-    
-    if(!se)
-        res.status(400).send({message: "Webhook not found!"});
+  Webhooks.forEach((webhook) => {
+    if (webhook.name === name) {
+      Webhooks.splice(j, 1);
+      res.status(200).send({message: 'Webhook deleted!'});
+      se = true;
+    }
+    j++;
+  });
+
+  if (!se) {
+    res.status(400).send({message: 'Webhook not found!'});
+  }
 });
 
 module.exports = router;
